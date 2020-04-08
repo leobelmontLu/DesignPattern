d6ea192174450e756894fc5babf44f5d2972d688
leobelmont <87540409@qq.com>
2020-04-09 00:11:13 +0800

    增加建造者模式的代码示例。 PS：建造者模式和工厂模式的区别在于建造者模式更加关注于零件的装配顺序

---
+++ BuilderPattern/BuilderPattern.cpp
@@ -0,0 +1,65 @@
+﻿#include "BuilderPattern.h"
+
+
+void Meal::addItem(Item* item)
+{
+	if (item)
+	{
+		itemList.push_back(item);
+	}
+}
+
+float Meal::getCost()
+{
+	float cost = 0.0f;
+	ItemList::iterator iter = itemList.begin();
+	for (; iter != itemList.end(); iter++)
+	{
+		cost += (*iter)->price();
+	}
+	return cost;
+}
+
+void Meal::showItems()
+{
+	ItemList::iterator iter = itemList.begin();
+	for (; iter != itemList.end(); iter++)
+	{
+		cout << "Item : " << (*iter)->GetItemName() << ", Packing : " << (*iter)->GetPacking()->pack() << ", Price : " << (*iter)->price() << endl;
+	}
+}
+
+bool MealBuilder::prepareVegMeal(Meal& meal)
+{
+	meal.addItem(new VegBurger());
+	meal.addItem(new Coke());
+	return true;
+}
+
+bool MealBuilder::prepareNonVegMeal(Meal& meal)
+{
+	meal.addItem(new ChickenBurger());
+	meal.addItem(new Pepsi());
+	return true;
+}
+
+int main()
+{
+	MealBuilder mealBuilder;
+
+	Meal vegMeal;
+	if (mealBuilder.prepareNonVegMeal(vegMeal))
+	{
+		cout << "Veg Meal" << endl;
+		vegMeal.showItems();
+		cout << "Total Cost:" << vegMeal.getCost() << endl;
+	}
+	cout << "\n" << endl;
+	Meal nonVegMeal;
+	if (mealBuilder.prepareNonVegMeal(nonVegMeal))
+	{
+		cout << "Non Veg Meal" << endl;
+		nonVegMeal.showItems();
+		cout << "Total Cost:" << nonVegMeal.getCost() << endl;
+	}
+}
---
+++ BuilderPattern/BuilderPattern.h
@@ -0,0 +1,173 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+//食物包装
+class Packing
+{
+public:
+	Packing() {};
+	~Packing() {};
+	virtual string pack() = 0;
+};
+
+//食物条目
+class  Item
+{
+public:
+	Item() {};
+	~Item() {};
+	virtual string GetItemName() = 0;
+	virtual Packing* GetPacking() = 0;
+	virtual float price() = 0;
+};
+
+//包装类实现
+class Wrapper : public Packing
+{
+public:
+	Wrapper() {};
+	~Wrapper() {};
+	virtual string pack()
+	{
+		return "Wrapper";
+	}
+};
+
+class Bottle : public Packing
+{
+public:
+	Bottle() {};
+	~Bottle() {};
+	virtual string pack()
+	{
+		return "Bottle";
+	}
+};
+
+//食品条目实现
+class Burger : public Item
+{
+public:
+	Burger() {};
+	~Burger() {};
+
+	virtual string GetItemName() = 0;
+	virtual Packing* GetPacking()
+	{
+		return new Wrapper();
+	}
+	virtual float price() = 0;
+};
+
+class ColdDrink : public Item
+{
+public:
+	ColdDrink() {};
+	~ColdDrink() {};
+
+	virtual string GetItemName() = 0;
+	virtual Packing* GetPacking()
+	{
+		return new Bottle();
+	}
+	virtual float price() = 0;
+};
+
+class VegBurger : public Burger
+{
+public:
+	VegBurger() {};
+	~VegBurger() {};
+
+	virtual string GetItemName()
+	{
+		return "Veg Burger";
+	}
+
+	virtual float price()
+	{
+		return 25.0f;
+	}
+};
+
+class ChickenBurger : public Burger
+{
+public:
+	ChickenBurger() {};
+	~ChickenBurger() {};
+
+	virtual string GetItemName()
+	{
+		return "Chicken Burger";
+	}
+
+	virtual float price()
+	{
+		return 50.5f;
+	}
+};
+
+class Coke : public ColdDrink
+{
+public:
+	Coke() {};
+	~Coke() {};
+
+	virtual string GetItemName()
+	{
+		return "Coke";
+	}
+
+	virtual float price()
+	{
+		return 30.0f;
+	}
+};
+
+class Pepsi : public ColdDrink
+{
+public:
+	Pepsi() {};
+	~Pepsi() {};
+
+	virtual string GetItemName()
+	{
+		return "Pepsi";
+	}
+
+	virtual float price()
+	{
+		return 35.5f;
+	}
+};
+
+typedef list<Item*> ItemList;
+class Meal
+{
+public:
+	Meal() {};
+	~Meal() {};
+	void addItem(Item* item);
+	float getCost();
+	void showItems();
+
+private:
+	ItemList itemList;
+};
+
+
+class MealBuilder
+{
+public:
+	MealBuilder() {};
+	~MealBuilder() {};
+
+	bool prepareVegMeal(Meal& meal);
+	bool prepareNonVegMeal(Meal& meal);
+
+};
