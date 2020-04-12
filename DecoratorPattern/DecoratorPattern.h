c9cfc298c793eaf42c0919caef822489a6397d86
leobelmont <87540409@qq.com>
2020-04-13 00:44:43 +0800

    增加装饰器模式 这种模式创建了一个装饰类，用来包装原有的类，并在保持类方法签名完整性的前提下，提供了额外的功能。

---
+++ DecoratorPattern/DecoratorPattern.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+
+using namespace std;
+
+class Shape
+{
+public:
+	virtual void Draw() = 0;
+};
+
+class Rectangle : public Shape
+{
+public:
+	virtual void Draw();
+};
+
+class Circle : public Shape
+{
+public:
+	virtual void Draw();
+};
+
+class ShapeDecorator : public Shape
+{
+public:
+	ShapeDecorator(Shape* _shape);
+	virtual void Draw();
+	virtual void setsetRedBorder() = 0;
+protected:
+	Shape* decoratedShape;
+};
+
+class RedShapeDecorator : public ShapeDecorator
+{
+public:
+	RedShapeDecorator(Shape* _shape)
+		:ShapeDecorator(_shape)
+	{}
+
+	virtual void Draw();
+	virtual void setsetRedBorder();
+};
